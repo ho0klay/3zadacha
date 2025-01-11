b4a71af3f75b6d03eb767d3c869f37a0ed6887a3
ho0klay <hook1aay@gmail.com>
2025-01-11 14:53:07 +0300

    Update zd3ch2.py

--- zd3ch2.py
+++ zd3ch2.py
@@ -8,12 +8,13 @@ class Warrior:
 		self.stamina = 100
 
 
-	def attack(self):
+	def attack(self,):
 		if self.stamina > 0:
 			self.stamina -=10
 			return True
 		else:
-			return False	
+			return False
+				
 
 	def defend(self, damage):
 		if self.armor > 0:
@@ -24,21 +25,23 @@ class Warrior:
 		
 def battle(unit1, unit2):
 	while unit1.health > 10 and unit2.health > 10 :
-
 		action1 = random.choice(['attack', 'defend'])		
 		action2 = random.choice(['attack', 'defend'])
-
 		if action1 == 'attack' and action2 == 'attack':
-			damage1 = random.randint(10, 30)
-			damage2 = random.randint(10, 30)
-			unit1.health -= damage2
-			unit2.health -= damage1
+			if unit1.attack():
+				damage1 = random.randint(10, 30)
+				unit2.defend(damage1)
+			if unit2.attack():
+				damage2 = random.randint(10,30)	
+				unit1.defend(damage1)
 		elif action1 == 'attack' and action2 == 'defend':
-			damage = random.randint(10, 20)
-			unit2.defend(damage)
+			if unit1.attack():
+				damage = random.randint(10, 20)
+				unit2.defend(damage)
 		elif action1 == 'defend' and action2 == 'attack':
-			damage = random.randint(10, 20)
-			unit1.defend(damage)
+			if unit2.attack():
+				damage = random.randint(10, 20)
+				unit1.defend(damage)
 		print(f'{unit1.name}: HP = {unit1.health}, Armor = {unit1.armor}, Stamina = {unit1.stamina}')
 		print(f'{unit1.name}: HP = {unit2.health}, Armor = {unit2.armor}, Stamina = {unit2.stamina}')
 	winner = unit1 if unit1.health > unit2.health else unit2
@@ -46,6 +49,6 @@ def battle(unit1, unit2):
 
 unit1 = Warrior('Voin 1')
 unit2 = Warrior('Voin2')
-battle(unit1, unit2)		
+battle(unit1, unit2)
 
 		
